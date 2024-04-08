029351643cf6c5a36ed9445f1f671fcfd268abf8
atahantutar <atahantutar69@gmail.com>
2024-04-08 20:18:13 +0300

    Add Auth component for authentication functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "./pages/mainpage/MainPage";
+import Auth from "./pages/auth/Auth";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </BrowserRouter>
   );
