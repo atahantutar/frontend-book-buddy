af3a74c99e565fe91da72dcfcb6d852dd9ed40ec
atahantutar <atahantutar69@gmail.com>
2024-04-25 18:29:03 +0300

    Refactor App.js to use React Router for routing and add Login and Register components for user authentication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Home from "./pages/mainpage/MainPage";
-import Auth from "./pages/auth/Auth";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
