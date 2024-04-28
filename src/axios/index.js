c8527529671a88bb41550ed02ec7b88b488df340
atahantutar <atahantutar69@gmail.com>
2024-04-28 15:34:23 +0300

    Refactor axios index.js to include new API endpoints for user information and book retrieval

--- src/axios/index.js
+++ src/axios/index.js
@@ -5,8 +5,16 @@ const HTTP = axios.create({
 });
 
 export const Login = async (FormData) => {
-  return await HTTP.post("/login", FormData);
+  return await HTTP.post("/login", FormData, {});
 };
 export const Register = async (FormData) => {
   return await HTTP.post("/register", FormData);
+};
+
+export const userInfo = async (id) => {
+  return await HTTP.post(`/userInfo/${id}`);
+};
+
+export const getBooks = async () => {
+  return await HTTP.get("/getbooks");
 };
