84eea5ae2249930343fa9646ccecbb8fa28e6bea
atahantutar <atahantutar69@gmail.com>
2024-05-03 22:56:02 +0300

    Add Header component to Login page

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,8 +1,14 @@
 import React from "react";
 import LoginForm from "../../components/auth/loginForm";
+import Header from "../../components/header/header";
 
 const Login = () => {
-  return <LoginForm />;
+  return (
+    <>
+      <Header />
+      <LoginForm />
+    </>
+  );
 };
 
 export default Login;
