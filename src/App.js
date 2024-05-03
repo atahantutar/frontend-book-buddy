3bbe186e94c3d6a9c80cb78430687e9ce9952610
atahantutar <atahantutar69@gmail.com>
2024-05-03 22:55:05 +0300

    Update routes in App.js to include new AddBook component and AuthProvider

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
 import AddBook from "./pages/books/addBook";
+import AuthProvider from "./context/authContext";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/addBook" element={<AddBook />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/add" element={<AddBook />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
