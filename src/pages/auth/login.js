df0dcc84beaa59ba3c5f1b16b6d1e6575545362a
atahantutar <atahantutar69@gmail.com>
2024-05-05 21:01:09 +0300

    feat: Add react-hot-toast for displaying toast messages

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,11 +1,13 @@
 import React from "react";
 import LoginForm from "../../components/auth/loginForm";
 import Header from "../../components/header/header";
+import { Toaster } from "react-hot-toast";
 
 const Login = () => {
   return (
     <>
       <Header />
+      <Toaster />
       <LoginForm />
     </>
   );
