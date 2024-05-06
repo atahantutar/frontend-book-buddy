b2dc187e3d8d26eb8a29bce934136a4e26344ae4
atahantutar <atahantutar69@gmail.com>
2024-05-06 17:32:12 +0300

    refactor: Remove react-hot-toast from login page

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,13 +1,11 @@
 import React from "react";
 import LoginForm from "../../components/auth/loginForm";
 import Header from "../../components/header/header";
-import { Toaster } from "react-hot-toast";
 
 const Login = () => {
   return (
     <>
       <Header />
-      <Toaster />
       <LoginForm />
     </>
   );
