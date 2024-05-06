5d328fdffcce128dc8911c9bf5a34a29e12bd850
atahantutar <atahantutar69@gmail.com>
2024-05-06 17:33:17 +0300

    refactor: Add Swal CSS for displaying success and error messages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/sweetalert2/dist/sweetalert2.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
