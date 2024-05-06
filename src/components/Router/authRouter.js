b4e4388abc29af0805e32b7b369c45f8838de747
atahantutar <atahantutar69@gmail.com>
2024-05-06 17:32:19 +0300

    feat: Add AuthRouter component for authentication routing

---
+++ src/components/Router/authRouter.js
@@ -0,0 +1,13 @@
+import Cookies from "universal-cookie";
+import { Navigate } from "react-router-dom";
+const AuthRouter = ({ children }) => {
+  const cookies = new Cookies();
+
+  if (!cookies.get("AccessToken")) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default AuthRouter;
