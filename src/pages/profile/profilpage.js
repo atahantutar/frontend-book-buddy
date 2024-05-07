aa4368cc5f0494099176871699d2b70d3ab445d6
atahantutar <atahantutar69@gmail.com>
2024-05-07 23:18:26 +0300

    refactor: Update ProfilePage component to use Header and Sidebar components

---
+++ src/pages/profile/profilpage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../../components/sideBar/sidebar";
+import Header from "../../components/header/header";
+
+const ProfilePage = () => {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+    </>
+  );
+};
+
+export default ProfilePage;
