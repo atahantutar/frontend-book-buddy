e0873f07f5d77aed465d7b199ed24248ad422189
atahantutar <atahantutar69@gmail.com>
2024-05-09 23:43:44 +0300

    refactor: Add ProfileProvider to App.js for managing user profile context

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
 import AuthProvider from "./context/authContext";
+import ProfileProvider from "./context/profileContext";
 import AddBookPage from "./pages/books/addBook";
 import AuthRouter from "./components/Router/authRouter";
 import ProfilePage from "./pages/profile/profilpage";
@@ -12,21 +13,23 @@ const App = () => {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route
-            path="/addBook"
-            element={
-              <AuthRouter>
-                <AddBookPage />
-              </AuthRouter>
-            }
-          />
-          <Route path="*" element={<h1>Not Found</h1>} />
-        </Routes>
+        <ProfileProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route
+              path="/addBook"
+              element={
+                <AuthRouter>
+                  <AddBookPage />
+                </AuthRouter>
+              }
+            />
+            <Route path="*" element={<h1>Not Found</h1>} />
+          </Routes>
+        </ProfileProvider>
       </AuthProvider>
     </BrowserRouter>
   );
