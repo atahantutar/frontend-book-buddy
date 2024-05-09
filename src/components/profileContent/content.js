fd063eb33a84501f8aa7e7793f368d42030d9003
atahantutar <atahantutar69@gmail.com>
2024-05-09 23:44:05 +0300

    refactor: Add Content component for displaying different sections in the profile page

---
+++ src/components/profileContent/content.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Profile from "./profile";
+import Books from "./userBooks";
+import Requests from "./bookRequests";
+import Swaps from "./bookSwaps";
+
+import { useContent } from "../../context/profileContext";
+
+const Content = () => {
+  const { stateContent } = useContent();
+
+  return (
+    <div className="col-9">
+      {stateContent.stateContent === "profile" && <Profile />}
+      {stateContent.stateContent === "books" && <Books />}
+      {stateContent.stateContent === "requests" && <Requests />}
+      {stateContent.stateContent === "swaps" && <Swaps />}
+    </div>
+  );
+};
+
+export default Content;
