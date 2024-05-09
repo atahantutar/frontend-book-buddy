7292b822765a6739eca666e3b77bdf0aa4d34866
atahantutar <atahantutar69@gmail.com>
2024-05-09 23:43:00 +0300

    refactor: Update Sidebar component to include navigation buttons for different content sections

--- src/components/sideBar/sidebar.js
+++ src/components/sideBar/sidebar.js
@@ -1,21 +1,61 @@
 import React from "react";
+import { useContent } from "../../context/profileContext";
 
 const Sidebar = () => {
+  const { setStateContent } = useContent();
   return (
-    <div className="col-2 min-vh-100 bg-dark text-white p-4">
+    <div className="col-2 min-vh-100  bg-dark text-white p-4">
       <div className="row mb-3">
-        <div className="col-12">
-          <h5>Books</h5>
+        <div className="col-2 w-100 ">
+          <button
+            className="btn btn-outline-secondary w-100"
+            onClick={() => setStateContent({ stateContent: "profile" })}
+          >
+            Profile
+          </button>
         </div>
       </div>
       <div className="row mb-3">
-        <div className="col-12">
-          <h5>Swap</h5>
+        <div className="col-2 w-100">
+          <button
+            className="btn btn-outline-secondary w-100"
+            onClick={() =>
+              setStateContent({
+                stateContent: "books",
+              })
+            }
+          >
+            Books
+          </button>
         </div>
       </div>
       <div className="row mb-3">
-        <div className="col-12">
-          <h5>Request</h5>
+        <div className="col-2 w-100">
+          <button
+            className="btn btn-outline-secondary w-100"
+            onClick={() =>
+              setStateContent({
+                stateContent: "requests",
+              })
+            }
+          >
+            Requests
+          </button>
+        </div>
+      </div>
+      <div className="row mb-3">
+        <div className="col-2 w-100 ">
+          <button
+            className="btn btn-outline-secondary w-100"
+            onClick={() => setStateContent({ stateContent: "swaps" })}
+          >
+            Swaps
+          </button>
+        </div>
+      </div>
+      <div className="row mt-auto">
+        <div className="col-2 w-100 ">
+          <span className="btn btn-outline-secondary w-100 ">User Info</span>
         </div>
       </div>
     </div>
