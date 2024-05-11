7df45b80baf326a65f98961025e24d4fbe493ecf
atahantutar <atahantutar69@gmail.com>
2024-05-11 18:56:02 +0300

    refactor: Update axios module to include authorization headers for userInfo, addBook, addAuthor, and addCategory functions

--- src/axios/index.js
+++ src/axios/index.js
@@ -7,12 +7,6 @@ const HTTP = axios.create({
   baseURL: "http://localhost:5500/",
 });
 
-const headers = {
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${cookies.get("AccessToken")}`,
-  },
-};
 export const Login = async (FormData) => {
   return await HTTP.post("/login", FormData);
 };
@@ -21,11 +15,16 @@ export const Register = async (FormData) => {
 };
 
 export const userInfo = async (id) => {
-  return await HTTP.post(`/userInfo/${id}`, headers);
+  return await HTTP.post(`/userInfo/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${cookies.get("AccessToken")}`,
+    },
+  });
 };
 
 export const addBook = async (FormData) => {
-  return await HTTP.post("/addbook", FormData, headers);
+  return await HTTP.post("/addbook", FormData);
 };
 export const getBooks = async () => {
   return await HTTP.get("/getbooks");
@@ -34,20 +33,110 @@ export const getAuthors = async () => {
   return await HTTP.get("/authors");
 };
 export const addAuthor = async (FormData) => {
-  return await HTTP.post("/addAuthor", FormData, headers);
+  return await HTTP.post("/addAuthor", FormData, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${cookies.get("AccessToken")}`,
+    },
+  });
 };
 export const getCategories = async () => {
   return await HTTP.get("/categories");
 };
 
 export const addCategory = async (FormData) => {
-  return await HTTP.post("/addCategory", FormData, headers);
+  return await HTTP.post("/addCategory", FormData, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${cookies.get("AccessToken")}`,
+    },
+  });
 };
 
-export const swapQuery = async (FormData) => {
-  return await HTTP.post("/swap", FormData, headers);
+export const swapQuery = async (id) => {
+  return await HTTP.post(
+    "/swap",
+    { id: id },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies.get("AccessToken")}`,
+      },
+    }
+  );
 };
 
-export const getUserBook = async (FormData) => {
-  return await HTTP.post(`/user/Books`, FormData, headers);
+export const getUserBook = async () => {
+  return await HTTP.post(
+    `/user/Books`,
+    {},
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies.get("AccessToken")}`,
+      },
+    }
+  );
+};
+
+export const deleteBook = async (Params) => {
+  return await HTTP.delete(
+    `/books/${Params.id}`,
+    {},
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies.get("AccessToken")}`,
+      },
+    }
+  );
+};
+
+export const userSwapRequests = async () => {
+  return await HTTP.post(
+    `/user/acceptorSwap`,
+    {},
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies.get("AccessToken")}`,
+      },
+    }
+  );
+};
+export const userOfferRequests = async () => {
+  return await HTTP.post(
+    `/user/offerSwap`,
+    {},
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies.get("AccessToken")}`,
+      },
+    }
+  );
+};
+export const rejectRequest = async (Params) => {
+  return await HTTP.post(
+    `/user/rejectSwap/${Params}`,
+    { Params: Params },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies.get("AccessToken")}`,
+      },
+    }
+  );
+};
+export const swapResponse = async (Params, Status) => {
+  return await HTTP.post(
+    `/user/swapResponse/${Params}`,
+    { status: Status },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies.get("AccessToken")}`,
+      },
+    }
+  );
 };
