6b160d31fce7f905fb848dbbad873f2a12ce031d
atahantutar <atahantutar69@gmail.com>
2024-05-17 16:23:30 +0300

    refactor: Update App.js to include lazy loading for images

--- src/App.js
+++ src/App.js
@@ -1,41 +1,51 @@
 import React from "react";
-import Home from "./pages/mainpage/MainPage";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/auth/login";
-import Register from "./pages/auth/register";
+
 import AuthProvider from "./context/authContext";
 import ProfileProvider from "./context/profileContext";
+import SearchTextProvider from "./context/searchTextContext";
+import BooksProvider from "./context/booksContext";
+
+import Home from "./pages/mainpage/MainPage";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
 import AddBookPage from "./pages/books/addBook";
-import AuthRouter from "./components/Router/authRouter";
 import ProfilePage from "./pages/profile/profilpage";
+
+import AuthRouter from "./components/Router/authRouter";
 
 const App = () => {
   return (
     <BrowserRouter>
       <AuthProvider>
         <ProfileProvider>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route
-              path="/profile"
-              element={
-                <AuthRouter>
-                  <ProfilePage />
-                </AuthRouter>
-              }
-            />
-            <Route
-              path="/addBook"
-              element={
-                <AuthRouter>
-                  <AddBookPage />
-                </AuthRouter>
-              }
-            />
-            <Route path="*" element={<h1>Not Found</h1>} />
-          </Routes>
+          <BooksProvider>
+            <SearchTextProvider>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                  path="/profile"
+                  element={
+                    <AuthRouter>
+                      <ProfilePage />
+                    </AuthRouter>
+                  }
+                />
+                <Route
+                  path="/addBook"
+                  element={
+                    <AuthRouter>
+                      <AddBookPage />
+                    </AuthRouter>
+                  }
+                />
+                <Route path="*" element={<h1>Not Found</h1>} />
+              </Routes>
+            </SearchTextProvider>
+          </BooksProvider>
         </ProfileProvider>
       </AuthProvider>
     </BrowserRouter>
